990e2e29a8df786e890e888dba507ff3ce4c059e
anncfs <80477584+anncfs@users.noreply.github.com>
2021-04-06 14:38:17 +0100

    Add enrichment analysis fns

---
+++ R/function_analysis.R
@@ -0,0 +1,47 @@
+## functional analysis functions
+
+#' Fisher's exact test
+#'
+#' wrapper function for R's fisher.test function to test each cluster identified in graph for overlap with genes of interest
+#'
+#' @param net_clusters Named list of cluster membership for each gene
+#' @param int_genes List of all genes in category of interest (any not in reference list are ignored)
+#' @param min_size Min size of cluster to analyse for overlap (default = 10)
+#' @param all_genes List of all genes to consider as reference list (i.e. full population under analysis, default = NULL in
+#'   which case all nodes in net_clusters used)
+#' @param filename Filename to write results to, default = NULL (no file saved)
+#'
+#' @export
+#'
+fisher_test_clusters <- function(net_clusters, int_genes, min_size = 10, all_genes = NULL, filename = NULL) {
+
+  # check int_genes and discard any not in reference list
+  if (is.null(all_genes)) { all_genes = names(net_clusters) }
+  int_genes = int_genes[which(int_genes %in% all_genes)]
+
+  # get list of cluster IDs above min size
+  idx = which(table(net_clusters) >= min_size)
+
+  # table for results
+  mat = matrix(ncol = 3, nrow = length(idx), dimnames = list(idx, c("cluster", "p-value", "p-adjust")))
+  mat[,1] = idx
+
+  for (i in idx) {
+
+    i_genes = names(which(net_clusters == i))
+
+    dfx = data.frame(cluster_i = all_genes %in% i_genes, int_genes = all_genes %in% int_genes)
+
+    x = stats::fisher.test(table(dfx), alternative = "greater")
+
+    mat[which(idx == i),2] = x$p.value
+  }
+
+  mat[,3] = stats::p.adjust(mat[,2], method = "BH")
+
+  if (!is.null(filename)) {
+    utils::write.table(round(mat, 4), file = filename, sep = "\t", quote = FALSE, row.names = FALSE)
+  }
+
+  return(mat)
+}
---
+++ R/go_enrichment.R
@@ -0,0 +1,63 @@
+## Functions for GO term analysis
+
+#' GO term enrichment of network communities
+#'
+#' Use WebGestaltR package to do GO term over-representation analysis for each community in network
+#'
+#' @param net_clusters Named list of community membership
+#' @param min_size minimum size of cluster to do GO enrichment for
+#' @param ref_genes list of genes to use as reference for GO enrichment (default = NULL, in which case
+#'   full genome used)
+#' @param ontology which ontology to use (any valid WebGestalt enrichDatabase,
+#'   default = 'geneontology_Biological_Process')
+#' @param filename filename to record summarised results
+#' @param dirname directory for WebGestaltR to output results (default = NULL, detailed output suppressed)
+#'
+#' @export
+#'
+go_enrichment <- function(net_clusters, min_size = 10,
+                          ontology = 'geneontology_Biological_Process',
+                          filename = NULL,
+                          ref_genes = NULL,
+                          dirname = NULL) {
+
+  # check output
+  if (is.null(dirname)) {
+    isOutput = FALSE
+    dirname = getwd()
+  } else {
+    isOutput = TRUE
+  }
+
+  # create directory if doesn't exist
+  if (!file.exists(dirname)) {dir.create(dirname)}
+
+  # get list of cluster IDs above min size
+  idx = which(table(net_clusters) >= min_size)
+
+  for (i in idx) {
+
+    int_genes = names(which(net_clusters == i))
+
+    enrichResult = WebGestaltR::WebGestaltR(enrichMethod = "ORA",
+                                            organism = "mmusculus", enrichDatabase = ontology,
+                                            interestGene = int_genes, interestGeneType = "genesymbol",
+                                            referenceGene = ref_genes, referenceGeneType = "genesymbol",
+                                            referenceSet = "genome",
+                                            isOutput = isOutput, outputDirectory = dirname,
+                                            projectName = paste0("cluster",i))
+
+    # (if no sig terms by fdr, then WebGestaltR returns NULL)
+    if (!is.null(enrichResult)) {
+      write(paste0("Cluster ", i, " (top FDR):"), file = filename, append = T)
+      gdata::write.fwf(enrichResult[1:10, c("geneSet", "description", "overlap", "enrichmentRatio", "pValue", "FDR")],
+                     file = filename, rownames = F, append = T)
+      write(paste0("\nCluster ", i, " (top enrichmentRatio):"), file = filename, append = T)
+      temp = enrichResult[order(-enrichResult$enrichmentRatio),]
+      gdata::write.fwf(temp[1:10, c("geneSet", "description", "overlap", "enrichmentRatio", "pValue", "FDR")],
+                     file = filename, rownames = F, append = T)
+      write('\n\n', file = filename, append = T)
+    }
+  }
+}
+
